b54312d070acce3002fe895a8d60d2be4cf8fed5
shadowridgedev <bruce@shadowridgedev.com>
2023-11-24 13:58:52 -0500

    linux

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from multiprocessing import Pool
 import mysql.connector
 import re
 
-## broken
+## broken again
 def sanitize_filename(title):
     # Replace invalid file name characters with an underscore
     return re.sub(r'[\\/:*?"<>|\|\t\n\r]', "_", title)
