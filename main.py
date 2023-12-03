886db5079508bd044f9bf919d5a30dd27a3597c0
shadowridgedev <bruce@shadowridgedev.com>
2023-12-02 20:46:56 -0500

    new sys

--- main.py
+++ main.py
@@ -1,47 +1,28 @@
-import sys
 import os
 import re
 import moviepy.editor as mp
 import speech_recognition as sr
-from pytube import YouTube
 from multiprocessing import Pool
 import mysql.connector
-
-# test
-## Function to adjust file paths for different operating systems
-def adjust_for_os(path):
-    return path.replace('/', os.sep) if os.name == 'nt' else path
-
 
 # Function to sanitize filenames
 def sanitize_filename(title):
     return re.sub(r'[\\/:*?"<>|\|\t\n\r]', "_", title)
 
-
-# Function to download videos from YouTube
-def download_video(video_url, download_path):
-    print(f"Downloading video from {video_url}...")
-    yt = YouTube(video_url)
-    video_title = sanitize_filename(yt.title)
-    file_name = f"{video_title}.mp4"
-    video = yt.streams.filter(file_extension='mp4').first()
-    video.download(output_path=download_path, filename=file_name)
-    print("Video downloaded successfully.")
-    return os.path.join(download_path, file_name)
-
-
-# Function to extract audio from video
+# Function to extract audio from video (if it's a video file)
 def extract_audio(video_path, download_path):
-    print(f"Extracting audio from {video_path}")
-    video_clip = mp.VideoFileClip(video_path)
-    audio_clip = video_clip.audio
-    audio_file_name = os.path.basename(video_path).replace('.mp4', '.wav')
-    audio_full_path = os.path.join(download_path, audio_file_name)
-    audio_clip.write_audiofile(audio_full_path)
-    audio_clip.close()
-    print("Audio extracted successfully.")
-    return audio_full_path
-
+    if video_path.endswith((".mp4", ".avi", ".mkv")):
+        print(f"Extracting audio from {video_path}")
+        video_clip = mp.VideoFileClip(video_path)
+        audio_clip = video_clip.audio
+        audio_file_name = os.path.basename(video_path).replace('.mp4', '.wav')
+        audio_full_path = os.path.join(download_path, audio_file_name)
+        audio_clip.write_audiofile(audio_full_path)
+        audio_clip.close()
+        print("Audio extracted successfully.")
+        return audio_full_path
+    else:
+        return None  # Not a video file, return None
 
 # Function to transcribe audio
 def transcribe_audio(segment):
@@ -61,7 +42,6 @@ def transcribe_audio(segment):
             print(f"Error in speech recognition: {e}")
             return f"[Error: {e}]"
 
-
 # Function to handle missing words in transcriptions
 def handle_missing_words(full_transcriptions, three_second_transcriptions, overlap):
     corrected_transcripts = []
@@ -79,7 +59,6 @@ def handle_missing_words(full_transcriptions, three_second_transcriptions, overl
     corrected_transcripts.append(full_transcriptions[-1])  # Append the last segment without modification
     return corrected_transcripts
 
-
 # Function to create a database and table
 def create_database_and_table(db_config):
     conn = mysql.connector.connect(**db_config)
@@ -89,8 +68,8 @@ def create_database_and_table(db_config):
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS YoutubeData (
             id INT AUTO_INCREMENT PRIMARY KEY,
-            video_url TEXT,
-            video_path TEXT,
+            file_name TEXT,
+            file_path TEXT,
             audio_path TEXT,
             transcript TEXT
         )
@@ -99,74 +78,60 @@ def create_database_and_table(db_config):
     cursor.close()
     conn.close()
 
-
 # Function to store data in database
-def store_data(db_config, video_url, video_path, audio_path, transcript):
+def store_data(db_config, file_name, file_path, audio_path, transcript):
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor()
     cursor.execute("USE youtube")
     cursor.execute("""
-        INSERT INTO YoutubeData (video_url, video_path, audio_path, transcript)
+        INSERT INTO YoutubeData (file_name, file_path, audio_path, transcript)
         VALUES (%s, %s, %s, %s)
-    """, (video_url, video_path, audio_path, transcript))
+    """, (file_name, file_path, audio_path, transcript))
     conn.commit()
     cursor.close()
     conn.close()
 
+# Function to process files in a directory
+def process_files(directory):
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        if os.path.isfile(filepath):
+            audio_path = extract_audio(filepath, download_path)
+            if audio_path:
+                segment_duration = 60
+                overlap = 3
+                total_duration = mp.VideoFileClip(filepath).duration
 
-# Function to read YouTube URLs from a file
-def read_urls_from_file(file_path):
-    with open(file_path, 'r') as file:
-        urls = file.read().splitlines()
-    return urls
+                full_segments = [(audio_path, i * segment_duration, segment_duration + overlap) for i in
+                                range(int(total_duration / segment_duration))]
+                three_second_segments = [(audio_path, i * segment_duration, overlap) for i in
+                                        range(1, int(total_duration / segment_duration))]
 
+                with Pool() as pool:
+                    full_transcriptions = pool.map(transcribe_audio, full_segments)
+                    three_second_transcriptions = pool.map(transcribe_audio, three_second_segments)
 
-# Main function
-def main():
-    youtube_urls_file = adjust_for_os("transcript.txt")  # Replace with your file path
-    youtube_urls = read_urls_from_file(youtube_urls_file)
+                corrected_transcripts = handle_missing_words(full_transcriptions, three_second_transcriptions, overlap)
 
-    download_path = adjust_for_os("E:/scratch")  # Replace with your download path
-
-
-    db_config = {
-            'host': os.environ.get('host', 'localhost'),
-            # Replace 'default_host' with a default value or leave as is
-            'port': int(os.environ.get('port', 3306)),  # Default MySQL port is 3306
-            'user': os.environ.get('user'),
-            'password': os.environ.get('password'),
-            'database': os.environ.get('database', 'youtube')
-        }
-
-        # ... [rest of your code] ...
-
-    create_database_and_table(db_config)
-
-    for video_url in youtube_urls:
-        try:
-            video_path = download_video(video_url, download_path)
-            audio_full_path = extract_audio(video_path, download_path)
-
-            segment_duration = 60
-            overlap = 3
-            total_duration = mp.VideoFileClip(video_path).duration
-
-            full_segments = [(audio_full_path, i * segment_duration, segment_duration + overlap) for i in
-                             range(int(total_duration / segment_duration))]
-            three_second_segments = [(audio_full_path, i * segment_duration, overlap) for i in
-                                     range(1, int(total_duration / segment_duration))]
-
-            with Pool() as pool:
-                full_transcriptions = pool.map(transcribe_audio, full_segments)
-                three_second_transcriptions = pool.map(transcribe_audio, three_second_segments)
-
-            corrected_transcripts = handle_missing_words(full_transcriptions, three_second_transcriptions, overlap)
-
-            full_transcript = "\n".join(corrected_transcripts)
-            store_data(db_config, video_url, video_path, audio_full_path, full_transcript)
-        except Exception as e:
-            print(f"Error processing {video_url}: {e}")
+                full_transcript = "\n".join(corrected_transcripts)
+                store_data(db_config, filename, filepath, audio_path, full_transcript)
+            else:
+                print(f"Skipping {filename} - Not a supported video format")
 
 
 if __name__ == "__main__":
-    main()
+    download_path = "audio_files"  # Directory to store extracted audio files
+    os.makedirs(download_path, exist_ok=True)
+
+    directory_to_scan = "/path/to/your/files"  # Replace with the directory containing your files
+    db_config = {
+        'host': os.environ.get('host', 'localhost'),
+        'port': int(os.environ.get('port', 3306)),
+        'user': os.environ.get('user'),
+        'password': os.environ.get('password', 'Rs232x25'),
+        'database': os.environ.get('database', '')
+    }
+
+    create_database_and_table(db_config)
+    process_files(directory_to_scan)
+
